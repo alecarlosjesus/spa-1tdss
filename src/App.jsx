ff7886c18ecf9945f213f6b23d7e7d2507bfbc01
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-30 19:13:54 -0300

    Iniciamos com Componentização e Props.

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Cabecalho from "./Components/Cabecalho";
+import Conteudo from "./Components/Conteudo";
+import Rodape from "./Components/Rodape";
+import logoReact from "./assets/react.svg";
+
+export default function App(){
+
+  //Criando props para ser transmitido para um dos componentes filhos.
+  let novoConteudo = "Nova notícia!";
+  
+  let altLogo = "Logomarca da Tecnologia React!";
+  
+  return(
+    <>
+      {/* Crie um Cabeçalho com um h1 um título e uma lista com 3 itens. */}
+        <Cabecalho/>
+      {/* Crie uma Section com uma div e 3 parágrafos com texto lorem de 3 linhas. */}
+      {/* Passando três props para o componente filho. */}
+        <Conteudo novoProps={novoConteudo} uriLogoReact={logoReact} altLogoReact={altLogo}/>
+      {/* Crie um rodapé com um p e o código do símbolo de copyright mais o texto que se segue. "Todos os meus direitos reservados. 2023" */}
+        <Rodape/>
+    </>
+  )
+}
