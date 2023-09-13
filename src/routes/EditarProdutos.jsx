8eac3a898bab6ea3864ab58fd7bd6e980685c599
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-13 19:46:54 -0300

    Refatorando o EditarProduto

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -5,20 +5,22 @@ export default function EditarProdutos() {
 
   document.title = "Editar Produtos";
 
-  //O HOOK useParams()
+  //Recuperando o ID com o HOOK useParams()
   const {id} = useParams();
 
-  const produtoRecuperadoPorId = ListaProdutos.filter((produto)=>{
-    if(produto.id == parseInt(id)){
-            return produto;
-        }
-      });
+  // const produtoRecuperadoPorId = ListaProdutos.filter((produto)=>{
+  //   if(produto.id == parseInt(id)){
+  //           return produto;
+  //       }
+  //     });
+
+  const produtoRecuperadoPorId = ListaProdutos.filter(item => item.id == id)[0];
   
   return (
     <>
       <h1>EditarProdutos</h1>
       <p>Produto selecionado - {id}</p>
-      <p>Produto selecionado - {produtoRecuperadoPorId[0].nome}</p>
+      <p>Produto selecionado - {produtoRecuperadoPorId.nome}</p>
     </>
 
 
