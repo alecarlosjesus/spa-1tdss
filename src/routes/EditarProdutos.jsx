0d725b271512c96b849b1208ceef7ed7b7236f1e
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-13 20:26:29 -0300

    EditarProduto - Preenchimento do useState.

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -4,34 +4,60 @@ import { useState } from "react";
 
 export default function EditarProdutos() {
 
+  document.title = `Editar Produtos` ;
  
 
   //Recuperando o ID com o HOOK useParams()
   const {id} = useParams();
 
-  // const produtoRecuperadoPorId = ListaProdutos.filter((produto)=>{
-  //   if(produto.id == parseInt(id)){
-  //           return produto;
-  //       }
-  //     });
 
   const produtoRecuperadoPorId = ListaProdutos.filter(item => item.id == id)[0];
 
-  const [count, setCount] = useState(0);
+  const [produto, setProduto] = useState({
+    id: produtoRecuperadoPorId.id,
+    nome: produtoRecuperadoPorId.nome,
+    preco: produtoRecuperadoPorId.preco
+  });
 
-  document.title = `Editar Produtos - ${count}` ;
+  const handleChangeProduto = (event)=> {
 
+        //Destructuring
+        const {name, value} = event.target;
+
+        //Popular o objeto produto com o setProduto() utilizando o operador SPREAD (...)
+        setProduto({...produto,[name]:value});
+
+  }
 
 
   return (
     <>
-      <h1>EditarProdutos</h1>
       <div>
-        <button onClick={()=> setCount( count+1)}>COUNTER - {count}</button>
+        <h1>Editar Produtos</h1>
+        <div>
+          <form>
+            <fieldset>
+              <legend>Produto Selecionado</legend>
+              <div>
+                <input type="hidden" name="id"/>
+              </div>
+              <div>
+                <label htmlFor="idNome">Nome:</label>
+                <input type="text" name="nome" id="idNome" onChange={handleChangeProduto} value={produto.nome}/>
+              </div>
+              <div>
+                <label htmlFor="idPreco">Preço:</label>
+                <input type="number" name="preco" id="idPreco" onChange={handleChangeProduto} value={produto.preco}/>
+              </div>
+              <div>
+                <button>EDITAR</button>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+        <p>Nome : {produto.nome}</p>
+        <p>Preço : {produto.preco}</p>
       </div>
-      <p>Produto selecionado - {id}</p>
-      <p>Produto selecionado - {produtoRecuperadoPorId.nome}</p>
-      <p>Valor do counter = {count}</p>
     </>
 
 
